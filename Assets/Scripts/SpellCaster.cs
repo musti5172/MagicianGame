4234aa72d7de639bbc5d05f057c9c2a8bb2e268d
musti5172 <m.tahakarakose2007bilgisayar@gmail.com>
2026-05-23 22:18:34 +0300

    ateş edebiliyoz güzel

--- Assets/Scripts/SpellCaster.cs
+++ Assets/Scripts/SpellCaster.cs
@@ -5,8 +5,13 @@ public class SpellCaster : MonoBehaviour
 
     public GameObject fireBall;
     public Transform firePoint;
-    public float fireSpeed = 10f;
+    public float fireSpeed = 30f;
     public Animator handAnimations;
+
+    [Header("Eldeki Fireball Ayarlarý")]
+    public GameObject fireBallOnHand;
+    public float growingSpeed = 3f;
+    public float maxSize = 1f;  
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -23,6 +28,15 @@ public class SpellCaster : MonoBehaviour
             handAnimations.SetTrigger("Shoot");
         }
 
+        if (fireBallOnHand.transform.localScale.x < maxSize)
+        {
+             float newSize  = fireBallOnHand.transform.localScale.x + (growingSpeed * Time.deltaTime);
+
+            newSize = Mathf.Clamp(newSize, 0, maxSize);
+
+            fireBallOnHand.transform.localScale = new Vector3(newSize, newSize, newSize);
+        }
+
     }
 
     void AtesEt()
@@ -31,5 +45,6 @@ public class SpellCaster : MonoBehaviour
         Rigidbody rb = yeniTop.GetComponent<Rigidbody>();
         rb.linearVelocity = firePoint.forward * fireSpeed;
         Destroy(yeniTop, 3f);
+        fireBallOnHand.transform.localScale = Vector3.zero;
     }
 }
